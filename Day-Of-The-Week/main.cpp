681f4c14f617c07a74368aa08640b2152c0faae3
V B Sakthivel <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2025-03-02 23:13:32 +0530

    Create main.cpp

---
+++ Day-Of-The-Week/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void display_day(int day_code) {
+    switch(day_code){
+        case 0:
+            cout << "Sunday";
+            break;
+        case 1:
+            cout << "Monday";
+            break;
+        case 2:
+            cout << "Tuesday";
+            break;
+        case 3:
+            cout << "Wednesday";
+            break;
+        case 4:
+            cout << "Thursday";
+            break;
+        case 5:
+            cout << "Friday";
+            break;
+        case 6:
+            cout << "Saturday";
+            break;
+        default:
+            cout << "Error - illegal day code";
+            break;
+    }
+}
+
+int main(){
+    int day_code;
+    cout << "Enter the day code: ";
+    cin >> day_code;
+    display_day(day_code);
+}
